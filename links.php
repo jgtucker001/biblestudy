b001b72aaa9c358285c68fc38b9595b4b817bcf7
jgtucker001 <jamestucker3@mac.com>
2023-08-06 17:52:56 -0400

    Adjusted grid layout for links

--- links.php
+++ links.php
@@ -4,20 +4,22 @@
 <body style="background-image:url('images/background.jpg')">
     <?php include("components/header.php") ?>
     <div id="home container">
-            <div class="heading3"><h2>EUCHARISTIC REVIVAL</h2></div> <br>
-            <div class="container">
-                <div class="card" style="width:60%">
-                    <ul class="list-group" style="font-size:1.5em">
-                        <li class="list-group-item"><a href="https://www.eucharisticrevival.org/" target=_blank>U.S. Bishops on Eucharistic Revival</a></li>
-                        <li class="list-group-item"><a href="https://diometuchen.org/eucharisticrevival#" target=_blank>Diocese of Metuchen on Eucharistic Revival</a></li>
-                        <li class="list-group-item"><a href="https://www.augustineinstitute.org/bible-studies/lectio-eucharist" target="_blank">Lectio Study: Eucharist</a></li>
-                    </ul>
-                </div>
+            <div class="heading3"><h2>RECOMMENDED BOOKS</h2></div> <br>
+      <div class="container">
+            <div class="row">
+                <div class="col-6">
+                 <div class="hdg"><h2>EUCHARISTIC REVIVAL</h2></div>
+                   <div class="card">
+                      <ul class="list-group" style="font-size:1.5em">
+                          <li class="list-group-item"><a href="https://www.eucharisticrevival.org/" target=_blank>U.S. Bishops on Eucharistic Revival</a></li>
+                          <li class="list-group-item"><a href="https://diometuchen.org/eucharisticrevival#" target=_blank>Diocese of Metuchen on Eucharistic Revival</a></li>
+                          <li class="list-group-item"><a href="https://www.augustineinstitute.org/bible-studies/lectio-eucharist" target="_blank">Lectio Study: Eucharist</a></li>
+                      </ul>
+                  </div>
             </div>
-            <br>
-            <div class="hdg"><h2>RECOMMENDED EUCHASRIST BOOKS</h2></div> <br>
-            <div class="container">
-                <div class="card" style="width:60%">
+            <div class="col-6">
+            <div class="hdg"><h2>RECOMMENDED EUCHASRIST BOOKS</h2></div>
+                <div class="card">
                     <ul class="list-group" style="font-size:1.5em">
                         <li class="list-group-item"><a href="https://tinyurl.com/mt6jrsv6" target=_blank>Jesus and the Jewish Roots of the Eucharist</a></li>
                         <li class="list-group-item"><a href="https://tinyurl.com/64w7k6mm" target=_blank>Worthy is the Lamb</a></li>
@@ -25,6 +27,7 @@
                     </ul>
                 </div>
             </div>
-    </div>
+            </div>
+            </div>
     <?php include("components/footer.php"); ?>
 </html>
