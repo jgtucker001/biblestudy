337945194e8b0d9a012dd5c3887ebb13c58f5f9f
jgtucker001 <jamestucker3@mac.com>
2023-11-20 13:54:52 -0500

    Added Messages

---
+++ messages/message.php
@@ -0,0 +1,4 @@
+<div class='container' style='color:red'>
+    <h4 style="font-size:large;font-weight:bold">New Resources:</h4>  
+    <p>Watch the Bible Timeline Show in the Links Section.</p>
+</div>
